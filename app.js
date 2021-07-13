d7708cd5b357b53a969adfcfa6c44368a026eac6
vbmn-cin-ufpe <vbmn@cin.ufpe.br>
2021-07-13 01:56:08 -0300

    Update app.js
    App.js foi atualizado

--- app.js
+++ app.js
@@ -5,13 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
   const scoreDisplay = document.querySelector('#score')
-  const startBtn = document.querySelector('#start-button')
-  const resetBtn = document.querySelector('#reset-button')
-  const onoffBtn = document.querySelector('#onoff-button')
+  const startBtn = document.querySelector('#start-button') 
+  const onOffBtn = document.querySelector('#onOff-button'); 
+  const resetBtn = document.querySelector("#reset-button");
   const width = 10
   let nextRandom = 0
   let timerId
   let score = 0
+
+  // Audios game  
+  let lineDrop = document.getElementById('lineDrop');    
+  let slowHit = document.getElementById('slowHit');
+  let audioRotation = document.getElementById('audioRotation'); 
+  let audioPause = document.getElementById('audioPause'); 
+  let audioGameOver = document.getElementById('audioGameOver');  
+  
   
   //The Tetrominoes
   const lTetromino = [
@@ -21,27 +29,12 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width*2, width*2+1, width*2+2]
   ]
 
-  const l2Tetromino = [
-    [1,width+1,width*2+1, width*2+2], //
-    [width, width+1, width+2, width*2], //
-    [2, 1, width+2, width*2+2],
-    [width+2, width*2, width*2+1, width*2+2] //
-  ]
-
   const zTetromino = [
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1],
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1]
   ]
-
-  const z2Tetromino = [
-    [1,width+1,width, width*2],
-    [width, width+1,width*2+1,width*2+2],
-    [1,width+1,width, width*2],
-    [width, width+1,width*2+1,width*2+2]
-  ]
-
 
   const tTetromino = [
     [1,width,width+1,width+2],
@@ -64,7 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
     [width,width+1,width+2,width+3]
   ]
 
-  const theTetrominoes = [lTetromino, l2Tetromino, zTetromino,  z2Tetromino, tTetromino, oTetromino, iTetromino]
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
   let currentRotation = 0
@@ -118,9 +111,10 @@ document.addEventListener('DOMContentLoaded', () => {
   var segundos = 1000
 
   //freeze function
-  function freeze() {
+  function freeze() { 
     if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
       current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+      slowHit.play();
       //start a new tetromino falling
       random = nextRandom
       nextRandom = Math.floor(Math.random() * theTetrominoes.length)
@@ -145,12 +139,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //move the tetromino right, unless is at the edge or there is a blockage
-  function moveRight() {
+  function moveRight() { 
     undraw()
     const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
     if(!isAtRightEdge) currentPosition +=1
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
-      currentPosition -=1
+      currentPosition -=1      
     }
     draw()
   }
@@ -191,6 +185,7 @@ document.addEventListener('DOMContentLoaded', () => {
     current = theTetrominoes[random][currentRotation]
     checkRotatedPosition()
     draw()
+    audioRotation.play();
   }
   /////////
 
@@ -204,10 +199,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //the Tetrominos without rotations
   const upNextTetrominoes = [
-    [1, displayWidth+1, displayWidth*2+1, 2], //l
-    [2, 1, width+2, width*2+2], //(l2) verificar se a rotação é semelhante a de cima
-    [0, displayWidth, displayWidth+1, displayWidth*2+1], //z
-    [0,width,width+1,width*2+1], //z2
+    [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
     [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
     [0, 1, displayWidth, displayWidth+1], //oTetromino
     [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
@@ -225,14 +218,35 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   }
   
+  //add functionality to the button ON OFF
+  onOffBtn.addEventListener('click',()=>{
+    //remove any trace of a tetromino form the entire grid
+    let nullValue = '';
+
+    //remove any trace of a tetromino form the entire grid
+    displaySquares.forEach(square => {
+      square.classList.remove('tetromino')
+      square.style.backgroundColor = ''
+    })
+
+    squares.forEach(index => {
+      if(index.hasAttribute('tetromino'))        
+        index.setAttribute('style', nullValue);
+        index.classList.remove('tetromino');      
+    })
+
+  })
+  
 
   
 
-  //add functionality to the button
-  startBtn.addEventListener('click', () => {
+  //add functionality to the button star
+  startBtn.addEventListener('click', () => {    
+
     if (timerId) {
       clearInterval(timerId)
       timerId = null
+      audioPause.play();
     } else {
       draw()
       timerId = setInterval(moveDown, segundos) //moveDown, 1000
@@ -241,18 +255,15 @@ document.addEventListener('DOMContentLoaded', () => {
       
     }
   })
-
-  resetBtn.addEventListener('click', () => {
-    location.reload()
+  
+  //add functionality to the button reset
+  resetBtn.addEventListener('click',()=>{
+    location.reload();
   })
-
-  onoffBtn.addEventListener('click', () => {
-
-  })
-
 
   //add score
   function addScore() {
+
     for (let i = 0; i < 199; i +=width) {
       const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
 
@@ -276,17 +287,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
         }
+        lineDrop.play();
       }
     }
     console.log(segundos)
   }
 
   //game over
-  function gameOver() {
+  function gameOver() {    
+    let audioTheme = document.getElementById('audioTheme');
+
     if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
       scoreDisplay.innerHTML = 'end'
       clearInterval(timerId)
+
+      let audioTheme = document.getElementById('audioTheme');
+      if(audioTheme.paused){
+        audioGameOver.play(); 
+      }else{
+        togglePlay(audioTheme);     
+        setTimeout(function(){ 
+          audioGameOver.play(); 
+        }, 500);
+      }      
+      
+      
+      //audioGameOver.play();
+
     }
   }
 
 })
+
+// AudioTheme
+function togglePlay(audioTheme) {
+  return audioTheme.paused ? audioTheme.play() : audioTheme.pause();
+};
+
+// Sound Theme of the Game
+function activateSound(){  
+  let audioTheme = document.getElementById('audioTheme');
+  togglePlay(audioTheme)  
+}
