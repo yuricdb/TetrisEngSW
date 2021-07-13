541f8649d16b71ae7c4e5d365378312e9f6f4377
Yuri Barros <50000444+yuricdb@users.noreply.github.com>
2021-07-13 16:37:07 -0300

    Update app.js

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
   let audioRotation = document.getElementById('audioRotation'); 
   let audioPause = document.getElementById('audioPause'); 
   let audioGameOver = document.getElementById('audioGameOver');  
-  
-  
+   
   //The Tetrominoes
   const lTetromino = [
     [1, width+1, width*2+1, 2],
@@ -30,11 +29,25 @@ document.addEventListener('DOMContentLoaded', () => {
     [width, width*2, width*2+1, width*2+2]
   ]
 
+  const l2Tetromino = [
+    [1,width+1,width*2+1, width*2+2], //
+    [width, width+1, width+2, width*2], //
+    [2, 1, width+2, width*2+2],
+    [width+2, width*2, width*2+1, width*2+2] //
+  ]
+
   const zTetromino = [
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1],
     [0,width,width+1,width*2+1],
     [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const z2Tetromino = [
+    [1,width+1,width, width*2],
+    [width, width+1,width*2+1,width*2+2],
+    [1,width+1,width, width*2],
+    [width, width+1,width*2+1,width*2+2]
   ]
 
   const tTetromino = [
@@ -58,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
     [width,width+1,width+2,width+3]
   ]
 
-  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  const theTetrominoes = [lTetromino, l2Tetromino, zTetromino,  z2Tetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
   let currentRotation = 0
@@ -99,6 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   document.addEventListener('keyup', control)
+  
 
   //move down function
   function moveDown() {
@@ -202,7 +216,9 @@ document.addEventListener('DOMContentLoaded', () => {
   //the Tetrominos without rotations
   const upNextTetrominoes = [
     [1, displayWidth+1, displayWidth*2+1, 2], //lTetromino
+    [2, 1, displayWidth+2, displayWidth*2+2], //(l2) verificar se a rotação é semelhante a de cima
     [0, displayWidth, displayWidth+1, displayWidth*2+1], //zTetromino
+    [1,displayWidth+1,displayWidth, displayWidth*2], //z2
     [1, displayWidth, displayWidth+1, displayWidth+2], //tTetromino
     [0, 1, displayWidth, displayWidth+1], //oTetromino
     [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
