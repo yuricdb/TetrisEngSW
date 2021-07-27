18309b1edbcf20d01139560dae085f560210dba9
Yuri Barros <yuricdb@gmail.com>
2021-07-27 11:48:09 -0300

    Prompt Resolvido
    Teste completamente automatizado, com "Prompt" funcionando, sendo inserido nome para cada teste.

--- cypress/integration/TestesTetris/todo.spec.js
+++ cypress/integration/TestesTetris/todo.spec.js
@@ -1,48 +1,56 @@
 /// <reference types="cypress" />
 
 const  teclas = ['{uparrow}','{downarrow}','{leftarrow}','{rightarrow}']
+const nomes = ['Yuri', 'Daniel', 'Victor'] //para cada um dos novos teste add, terá que ir add nomes aqui para entrar no alert, ex: 'giovanni', etc
 
-
-//Preciso descobrir como fazer o cypress digitar o nome do jogador
 describe('Teste Jogo Tetris', () => {
-  it('Verificando Todos os Botões', () => {
+    beforeEach(function() { cy.visit('http://127.0.0.1:5500/index.html')})
+    
+    it('Teste Botões - Alert', function(){
+        cy.window().then(function($win){
+            cy.viewport(1600,1080)
+            cy.stub($win, 'prompt').returns(nomes[0])
+            cy.get('#onOff-button').click();
+            cy.wait(3000)
+            cy.get('#sound-button').click();
+            cy.wait(3000)
+            cy.get('#start-button').click();
+            cy.wait(3000)
+            cy.get('#reset-button').click();
+            cy.wait(3000)
+        })
 
-    cy.visit('/')
-    cy.get('#onOff-button').click();
-    cy.wait(3000)
-    cy.get('#sound-button').click();
-    cy.wait(3000)
-    cy.get('#start-button').click();
-    cy.wait(3000)
-    cy.get('#reset-button').click();
-    cy.wait(3000)
-  })
+    })
 
-  it('Verificando Movimento das Peças', () => {
+    it('Teste Movimento das Peças - Alert', function(){
+        cy.window().then(function($win){
+            cy.viewport(1600,1080)
+            cy.stub($win, 'prompt').returns(nomes[1])
+            cy.get('#onOff-button').click()
+            cy.get('#start-button').click()
+            for(let passo = 0; passo < 20; passo++){
+                cy.get('body').type(teclas[parseInt(Math.random()*4)])
+                cy.wait(1000)
+              } 
+        })
 
-    cy.visit('/')
-    cy.get('#onOff-button').click();
-    cy.wait(1000)
-    cy.get('#start-button').click();
-    cy.wait(1000)
-    for(let passo = 0; passo < 20; passo++){
-      cy.get('body').type(teclas[parseInt(Math.random()*4)])
-      cy.wait(1000)
-    }   
-  }) 
+    })
 
-  it('Verificando o GameOver', () => {
+    it('Teste Game-Over - Alert', function(){
+        cy.window().then(function($win){
+            cy.viewport(1600,1080)
+            cy.stub($win, 'prompt').returns(nomes[2])
+            cy.get('#onOff-button').click()
+            cy.wait(1000)
+            cy.get('#start-button').click()
+            cy.wait(1000)
+            for(let passo = 0; passo < 70; passo++){
+                cy.get('body').type(teclas[1])       
+              } 
+              cy.get('#score').contains('span', 'end');
+              cy.get('#level').contains('span', 'end');
+        })
 
-      cy.visit('/')
-      cy.get('#onOff-button').click();
-      cy.wait(1000)
-      cy.get('#start-button').click();
-      cy.wait(1000)
-      for(let passo = 0; passo < 70; passo++){
-        cy.get('body').type(teclas[1])       
-      } 
-      cy.get('#score').contains('span', 'end');
-      cy.get('#level').contains('span', 'end');
-  })
-  })
- //Sugestão de Teste: Parar e Retornar Jogo, Reiniciar com video game desligado, Parar e Retornar Som, Tentar movimentar com jogo Pausado
+    })
+
+})
